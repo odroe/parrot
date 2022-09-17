ea090fb5433e6b99f9cdcccb9a1506632cadd520
Seven Du <whine.whitish.0u@icloud.com>
2022-09-17 12:10:04 +0800

    chore: public APIs exports

--- packages/parrot/lib/parrot.dart
+++ packages/parrot/lib/parrot.dart
@@ -1,7 +1,25 @@
 /// 🦜 Parrot libraries.
 library parrot.core;
 
+// Core base
 export 'src/parrot_application.dart';
 export 'src/parrot_context.dart';
+
+// Annotations
 export 'src/annotations/injectable.dart';
 export 'src/annotations/module.dart';
+
+// Container
+export 'src/container/parrot_container.dart';
+export 'src/container/parrot_token.dart';
+
+// Injector
+export 'src/injector/module_annotation.dart';
+export 'src/injector/module_compiler.dart';
+export 'src/injector/module_context.dart';
+export 'src/injector/provider_compiler.dart';
+export 'src/injector/provider_context.dart';
+export 'src/injector/scope.dart';
+
+// Utils
+export 'src/utils/typed_symbol.dart';
