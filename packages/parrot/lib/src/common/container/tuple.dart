5109ccf0f179b362ac76617c51bb218b87ba6593
ordself <82750922+ordself@users.noreply.github.com>
2022-09-23 23:56:20 +0800

    feat(parrot): add Tuple3 container

--- packages/parrot/lib/src/common/container/tuple.dart
+++ packages/parrot/lib/src/common/container/tuple.dart
@@ -16,3 +16,25 @@ class Tuple2<T1, T2> {
     return "Tuple2<${T1.toString()}, ${T2.toString()}>(first=${first.toString()}, second=${second.toString()})";
   }
 }
+
+class Tuple3<T1, T2, T3> {
+  const Tuple3(this.first, this.second, this.third);
+
+  final T1 first;
+
+  final T2 second;
+
+  final T3 third;
+
+  Tuple3<R1, R2, R3> flatMap<R1, R2, R3>(
+          Tuple3<R1, R2, R3> Function(T1 first, T2 second, T3 third) mapper) =>
+      mapper(first, second, third);
+
+  Tuple3<R1, R2, R3> cast<R1, R2, R3>() =>
+      Tuple3(first as R1, second as R2, third as R3);
+
+  @override
+  String toString() {
+    return "Tuple3<${T1.toString()}, ${T2.toString()}, ${T3.toString()}>(first=${first.toString()}, second=${second.toString()}, third=${third.toString()})";
+  }
+}
