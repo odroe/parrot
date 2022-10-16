1ef3aa0d63e586122bee5ee3962205ec9030df46
Seven Du <whine.whitish.0u@icloud.com>
2022-10-16 16:01:52 +0800

    chore: Remove demo

--- packages/parrot_mirror/lib/src/compiler/compiler.dart
+++ packages/parrot_mirror/lib/src/compiler/compiler.dart
@@ -1,5 +1,3 @@
-import 'dart:mirrors';
-
 import 'package:parrot/parrot.dart';
 
 abstract class Compiler implements ParrotCompiler {
@@ -8,14 +6,4 @@ abstract class Compiler implements ParrotCompiler {
 
   /// Returns current container.
   ParrotContainer get container => application.container;
-}
-
-Demo haha(a) => Demo();
-
-class Demo {}
-
-void main(List<String> args) {
-  final ClosureMirror demo = reflect(haha) as ClosureMirror;
-
-  print(demo.apply([1]).reflectee);
 }
